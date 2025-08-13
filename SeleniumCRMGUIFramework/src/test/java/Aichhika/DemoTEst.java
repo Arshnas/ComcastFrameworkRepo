ad27765c17671741113ecdb3c5427d7d005647dc
AICHHIKA <141505620+AICHHIKA@users.noreply.github.com>
2025-08-13 16:34:08 +0530

    Update DemoTEst.java

--- SeleniumCRMGUIFramework/src/test/java/Aichhika/DemoTEst.java
+++ SeleniumCRMGUIFramework/src/test/java/Aichhika/DemoTEst.java
@@ -5,5 +5,9 @@ public class DemoTEst {
 	public static void main(String[] args) {
 		
 		System.out.println("--- print ---");
+		System.out.println("--- print ---");
+		System.out.println("--- print ---");
+		
 	}
 }
+
